22151f42f9dc640bda1507c6a1bb2f24b16e32a8
Anthony Castro <anthony618@gmail.com>
2024-03-21 22:48:03 -0400

    Disable console lint rule

--- src/content-scripts/recipe-scraper/index.ts
+++ src/content-scripts/recipe-scraper/index.ts
@@ -1,1 +1,2 @@
+// eslint-disable-next-line no-console
 console.log("From recipe scraper: Hello world!");
--- src/service-worker/index.ts
+++ src/service-worker/index.ts
@@ -1,1 +1,2 @@
+// eslint-disable-next-line no-console
 console.log("From service worker: Hello World!");

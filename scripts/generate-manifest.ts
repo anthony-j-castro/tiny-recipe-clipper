1d000bc68a7ae5b2609d299b6ca3103d947d6ad3
Anthony Castro <anthony618@gmail.com>
2024-03-31 15:26:36 -0400

    Add version_name

--- scripts/generate-manifest.ts
+++ scripts/generate-manifest.ts
@@ -10,6 +10,11 @@ const main = async () => {
   const manifest = { ...baseManifest };
   manifest.version = packageJson.version;
 
+  if (process.env.GITHUB_SHA) {
+    const shortSha = process.env.GITHUB_SHA.substring(0, 7);
+    manifest.version_name = `${manifest.version}-${shortSha}`;
+  }
+
   const prettierOptions = await prettier.resolveConfig(
     path.resolve(__dirname, "placeholder.json"),
     {

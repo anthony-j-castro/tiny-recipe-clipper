8f60878fa7668b538f22e8d8b3513cdae370873a
Anthony Castro <anthony618@gmail.com>
2024-04-03 23:35:17 -0400

    Remove describe block

--- playwright/tests/scrapers/cooking.nytimes.com.spec.ts
+++ playwright/tests/scrapers/cooking.nytimes.com.spec.ts
@@ -1,38 +1,36 @@
 import { expect, test } from "~/playwright/fixtures";
 import TimesScraper from "~/src/content-scripts/recipe-scraper/scrapers/cooking.nytimes.com";
 
-test.describe("cooking.nytimes.com", () => {
-  test("parses a standard recipe", async ({
+test("parses a standard recipe", async ({
+  customExecuteInPageScope,
+  page,
+  recipeFixtures,
+}) => {
+  await page.goto("https://cooking.nytimes.com/recipes/7131-salmon-burgers");
+
+  const scraper = new TimesScraper({
     customExecuteInPageScope,
-    page,
-    recipeFixtures,
-  }) => {
-    await page.goto("https://cooking.nytimes.com/recipes/7131-salmon-burgers");
-
-    const scraper = new TimesScraper({
-      customExecuteInPageScope,
-    });
-
-    const recipe = await scraper.load();
-
-    expect(recipe).toEqual(recipeFixtures.salmonBurgers);
   });
 
-  test("parses a recipe with 2 attributions", async ({
+  const recipe = await scraper.load();
+
+  expect(recipe).toEqual(recipeFixtures.salmonBurgers);
+});
+
+test("parses a recipe with 2 attributions", async ({
+  customExecuteInPageScope,
+  page,
+  recipeFixtures,
+}) => {
+  await page.goto(
+    "https://cooking.nytimes.com/recipes/1023785-magnolia-bakerys-banana-pudding",
+  );
+
+  const scraper = new TimesScraper({
     customExecuteInPageScope,
-    page,
-    recipeFixtures,
-  }) => {
-    await page.goto(
-      "https://cooking.nytimes.com/recipes/1023785-magnolia-bakerys-banana-pudding",
-    );
-
-    const scraper = new TimesScraper({
-      customExecuteInPageScope,
-    });
-
-    const recipe = await scraper.load();
-
-    expect(recipe).toEqual(recipeFixtures.bananaPudding);
   });
+
+  const recipe = await scraper.load();
+
+  expect(recipe).toEqual(recipeFixtures.bananaPudding);
 });

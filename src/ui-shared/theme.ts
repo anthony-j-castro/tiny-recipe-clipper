bfc2a5b89dd53dd39db6ea9ce3f386eb4308f325
Anthony Castro <anthony618@gmail.com>
2024-04-05 17:31:34 -0400

    Tweak gray value to match web app

--- src/ui-shared/theme.ts
+++ src/ui-shared/theme.ts
@@ -3,7 +3,7 @@ const theme = {
     white: "#ffffff",
     nearBlack: "#202124",
     lightGray: "#f8f9fa",
-    gray: "#e1e5ea",
+    gray: "#e1e1e1",
     darkGray: "#616161",
     lightPurple: "#9f7fd9",
     purple: "#4100b3",

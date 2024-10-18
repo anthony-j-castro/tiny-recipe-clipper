9ec7ac396249100f618f4d7dff52fdb7abf6590e
Anthony Castro <anthony618@gmail.com>
2024-10-18 18:04:00 -0400

    Upgrade eslint

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import config from "@anthony-j-castro/eslint-config";
+import jsonc from "eslint-plugin-jsonc";
+import globals from "globals";
+import jsoncParser from "jsonc-eslint-parser";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default [
+  ...config,
+  {
+    ignores: ["dist/*", "playwright/report/**", "playwright/test-results/**"],
+  },
+  {
+    settings: {
+      "import/resolver": {
+        alias: {
+          map: [["~", path.resolve(__dirname, "src")]],
+          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        },
+        typescript: { project: "tsconfig.json" },
+      },
+    },
+  },
+  {
+    files: ["webpack.config.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["playwright/**/*.json"],
+    languageOptions: {
+      parser: jsoncParser,
+    },
+    plugins: { jsonc },
+    rules: {
+      "jsonc/sort-keys": "error",
+    },
+  },
+  {
+    files: ["playwright/**/*"],
+    settings: {
+      "import/resolver": {
+        alias: {
+          map: [
+            ["~/playwright", path.resolve(__dirname, "playwright")],
+            ["~/src", path.resolve(__dirname, "src")],
+          ],
+          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        },
+      },
+    },
+  },
+];

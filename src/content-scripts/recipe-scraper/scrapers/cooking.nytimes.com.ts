73c807818a7c4435d160ac88fcd31d6d0298c1f3
Anthony Castro <anthony618@gmail.com>
2024-10-20 21:35:13 -0400

    Simplify extracting attribution

--- src/content-scripts/recipe-scraper/scrapers/cooking.nytimes.com.ts
+++ src/content-scripts/recipe-scraper/scrapers/cooking.nytimes.com.ts
@@ -1,7 +1,6 @@
 import { formatDuration } from "date-fns";
-import { array, string } from "decoders";
+import { array, nonEmptyString, object, string } from "decoders";
 import { parse } from "iso8601-duration";
-import { RecipeParseError } from "~/errors";
 import { BaseScraper, type Executor, type LoadReturn, type Scraper } from ".";
 
 interface IngredientsGroup {
@@ -25,72 +24,15 @@ export default class TimesScraper extends BaseScraper implements Scraper {
   }
 
   async _getAttribution() {
-    try {
-      const attribution = await this._executeInPageScope(() => {
-        const attributionElements =
-          window.document.querySelectorAll<HTMLElement>('h2[class*="byline"]');
+    const author = object({ name: nonEmptyString }).value(
+      this.recipeJson.author,
+    );
 
-        if (attributionElements.length === 0) {
-          return null;
-        }
-
-        if (attributionElements.length > 2) {
-          throw new RecipeParseError(
-            "More than two lines of attribution detected.",
-          );
-        }
-
-        if (attributionElements.length === 1) {
-          const attributionText = attributionElements[0]?.innerText.trim();
-
-          if (!attributionText) {
-            return null;
-          }
-
-          const matches = attributionText.match(/^By\s*(.*)$/);
-
-          if (matches === null) {
-            return null;
-          }
-
-          const [attribution] = matches;
-
-          if (!attribution || matches.length !== 2) {
-            return null;
-          }
-
-          return attribution;
-        }
-
-        if (attributionElements.length === 2) {
-          const recipeFromText = attributionElements[0]?.innerText.trim();
-          const adaptedByText = attributionElements[1]?.innerText.trim();
-
-          if (!recipeFromText || !adaptedByText) {
-            return null;
-          }
-
-          if (
-            /^Recipe from\s*(.*)$/.test(recipeFromText) &&
-            /^Adapted by\s*(.*)$/.test(adaptedByText)
-          ) {
-            return `${recipeFromText}, ${adaptedByText}`;
-          }
-        }
-
-        return null;
-      });
-
-      return attribution;
-    } catch (error) {
-      if (error instanceof RecipeParseError) {
-        this.alerts.push(error.message);
-      } else {
-        throw error;
-      }
+    if (author === undefined) {
+      return null;
     }
 
-    return null;
+    return `By ${author.name}`;
   }
 
   async _getIngredientGroups(): Promise<IngredientsGroup[]> {

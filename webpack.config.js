0b722742f485a70be82b5adbe1c11832c6a10f70
Anthony Castro <anthony618@gmail.com>
2024-10-23 21:13:49 -0400

    Restore styled engine

--- webpack.config.js
+++ webpack.config.js
@@ -93,6 +93,7 @@ const config = {
   ],
   resolve: {
     alias: {
+      "@mui/styled-engine": "@mui/styled-engine-sc",
       "~": path.resolve(__dirname, "src"),
     },
     extensions: [".ts", ".tsx", ".js", ".jsx"],

e76bba994316983e09ecd17fe66d35f296e345eb
Anthony Castro <anthony618@gmail.com>
2026-08-16 19:32:42 -0400

    Tweak env

--- src/environment.ts
+++ src/environment.ts
@@ -5,12 +5,11 @@ const ENV = {
   BUILD_ENV:
     typeof process === "undefined"
       ? import.meta.env?.VITE_BUILD_ENV
-      : (process.env.BUILD_ENV ?? process.env.VITE_BUILD_ENV),
+      : process.env.VITE_BUILD_ENV,
   ROLLBAR_ACCESS_TOKEN:
     typeof process === "undefined"
       ? import.meta.env?.VITE_ROLLBAR_ACCESS_TOKEN
-      : (process.env.ROLLBAR_ACCESS_TOKEN ??
-        process.env.VITE_ROLLBAR_ACCESS_TOKEN),
+      : process.env.VITE_ROLLBAR_ACCESS_TOKEN,
 };
 
 export default ENV;
